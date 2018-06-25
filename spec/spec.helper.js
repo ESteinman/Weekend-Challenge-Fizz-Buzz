fff744959e2893d202f3fc8e52bfd28107feea14
Erik <erik.steinman@hotmail.com>
2018-06-25 08:41:54 +0200

    put index.html in docs

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -2,5 +2,5 @@ const fs = require ('fs');
 const chai = require ('chai');
 global.expect = chai.expect;
 
-let fizzBuzz = fs.readFileSync('./src/js/fizz-buzz.js');
+let fizzBuzz = fs.readFileSync('./docs/fizz-buzz.js');
 eval( fizzBuzz + `\nexports.FizzBuzz = FizzBuzz;`)
